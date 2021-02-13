23de086667c0fb5fd694d3b77456451cdfe4008b
root <root@gul.uc3m.es>
2021-02-13 18:31:50 +0100

    move iot to another repo and clean garbage

--- old_www.gul.uc3m.es/choomla/modules/mod_fortuc3ms/tmpl/default.php
+++
@@ -1,6 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<!-- div align="right" -->
-   <?php echo JText::_($fortune); ?>
-<!-- /div -->
-
--- old_www.gul.uc3m.es/old/Versions/3/conv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE=$1
-
-cp $FILE /tmp
-iconv -f iso-8859-1 -t utf-8 /tmp/$FILE > $FILE
-
