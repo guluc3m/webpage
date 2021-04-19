0be4a7ccd71da5011a498ca9b2e33230c025e386
IvanM1903 <martinivan120@gmail.com>
2021-04-19 21:15:12 +0200

    Nueva seccion agregada 'quien somos' + responsive para movil y escritorio

--- static/js/script.js
+++ static/js/script.js
@@ -13,9 +13,36 @@ $(document).ready(function(){
     var originalHeight = window.innerHeight;
     var offset = offsetNav();
     var newHeight = originalHeight - offset;
-    console.log(originalHeight);
-    console.log(offset);
-    console.log(newHeight);
+    //console.log("Altura original del cliente: "+originalHeight);
+    //console.log("Altura del nav: "+offset);
+    //console.log("Altura tras restarle el nav: "+newHeight);
     $("#what-is-gul-section").css("height",newHeight);
+
+    /* ====== CHANGE NAV COLOR ON SCROLL ====== */
+
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 700) {
+            $('.navbar').addClass('goDown');
+            $('.navbar').removeClass('bg-light');
+            //console.log("Ha bajado");
+        } else {
+            $('.navbar').removeClass('goDown');
+            $('.navbar').addClass('bg-light');
+            //console.log("Ha subido");
+        }
+      });
+
+
+    /* ====== MAKE DIVS APPEAR ON SCROLL ====== */
+    
+    $(window).scroll(function(){
+        if ($(document).scrollTop() > newHeight/2) {
+            $( '.text-content' ).removeClass( 'animate__backOutDown' ).show().addClass( ' animate__backInDown' );
+            $( '.img-content' ).removeClass( 'animate__backOutDown' ).show().addClass( ' animate__backInUp' );
+        } else {
+            $( '.text-content' ).removeClass( ' animate__backInDown' ).addClass( 'animate__backOutDown' );
+            $( '.img-content' ).removeClass( ' animate__backInUp' ).addClass( 'animate__backOutDown' );
+        }
+    });
 
 });
