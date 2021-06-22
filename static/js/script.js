e0babb91847e3cafd63af3f5a84b6020f75eb16a
IvanM1903 <martinivan120@gmail.com>
2021-06-22 17:24:00 +0200

    Creadas secciones who-we-are where-we-are y curiosity. Falta terminar contacto + footer + enlaces en nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,49 +1,29 @@
 
-$(document).ready(function(){
+function offsetNav() {
+    var navbar = document.getElementById("navbar");
+    var offset = navbar.offsetHeight;
+    return offset;
+}
+
+/* ====== SCROLL ANYWHERE ====== */
+function scrollToIt(section) {
+    console.log(section.toString());
+    var sectionHeight = $(section.toString()).offset().top;
+    var offset = 84;
+    console.log(sectionHeight);
+
+    var distance = sectionHeight - offset / 5;
+
+    $(window).scrollTop(distance);
+}
 
 
-    /* ====== OFFSET HEIGHT FUNCTION ====== */
-
-    function offsetNav(){
-        var navbar = document.getElementById("navbar");
-        var offset = navbar.offsetHeight;
-        return offset;
-    }
-
-    var originalHeight = window.innerHeight;
-    var offset = offsetNav();
-    var newHeight = originalHeight - offset;
-    //console.log("Altura original del cliente: "+originalHeight);
-    //console.log("Altura del nav: "+offset);
-    //console.log("Altura tras restarle el nav: "+newHeight);
-    $("#what-is-gul-section").css("height",newHeight);
-    $(".what-is-gul-card").css("margin-top",offset);
-    
-    /* ====== CHANGE NAV COLOR ON SCROLL ====== */
-
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 700) {
-            $('.navbar').addClass('goDown');
-            $('.navbar').removeClass('bg-light');
-            //console.log("Ha bajado");
-        } else {
-            $('.navbar').removeClass('goDown');
-            $('.navbar').addClass('bg-light');
-            //console.log("Ha subido");
-        }
-      });
-
-
-    /* ====== MAKE DIVS APPEAR ON SCROLL ====== */
-    
-    $(window).scroll(function(){
-        if ($(document).scrollTop() > newHeight/2) {
-            $( '.text-content' ).removeClass( 'animate__backOutDown' ).show().addClass( ' animate__backInDown' );
-            $( '.img-content' ).removeClass( 'animate__backOutDown' ).show().addClass( ' animate__backInUp' );
-        } else {
-            $( '.text-content' ).removeClass( ' animate__backInDown' ).addClass( 'animate__backOutDown' );
-            $( '.img-content' ).removeClass( ' animate__backInUp' ).addClass( 'animate__backOutDown' );
-        }
-    });
-
-});
+/* ====== OPEN STREET MAP  -  LEAFLET ====== */
+var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib });
+var map = L.map('map').setView([40.33139,-3.76821], 17).addLayer(osm);
+L.marker([40.33228,-3.76589])
+    .addTo(map)
+    .bindPopup('Universidad Carlos III de Madrid')
+    .openPopup();
