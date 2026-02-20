4b7b0a7b163c68b9a8d77d46ba073464680328a8
Miguel Monereo Cuéllar <46822584+Elmiguegv@users.noreply.github.com>
2026-02-20 11:39:09 +0100

    Update fortunas.js

--- static/js/fortunas.js
+++ static/js/fortunas.js
@@ -1169,5 +1169,9 @@ var fortunas = [
     [
         "Lo de consultarlo con la almohada y entregarlo el domingo es una mierda, a mi me lo entregais el viernes.\n¿Sabeis lo que les pone a los jefes eso? \nLes pone nivel se van al baño a tocarse.",
         "Daniel Garzon (Seguridad en dispositivos móviles)"
+    ],
+    [
+        "Me voy a auto proclamar rumano",
+        "Jorge Castañeda"
     ], 
 ]
